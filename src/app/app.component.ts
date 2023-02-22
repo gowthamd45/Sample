84d3b0226abe1878e1ad922e2399adad20691809
gowthamd45 <123611561+gowthamd45@users.noreply.github.com>
2023-02-21 20:31:37 -0600

    Sample Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { NgModule} from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import { FormsModule} from '@angular/forms'
+interface FileItem{
+  file:File;
+  checked:boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+export class FileUploadComponent implements OnInit {
+  fileItems: FileItem[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onDrop(event: any): void {
+    event.preventDefault();
+    const files: File[] = event.dataTransfer.files;
+    for (let i = 0; i < files.length; i++) {
+      this.fileItems.push({
+        file: files[i],
+        checked: false
+      });
+    }
+  }
+  onFileSelected(event: any): void {
+    const files: File[] = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      this.fileItems.push({
+        file: files[i],
+        checked: false
+      });
+    }
+  }
+  onCheckAll(event: any): void {
+    const checked = event.target.checked;
+    this.fileItems.forEach((item) => item.checked = checked);
+  }
+  onDeleteSelected(): void {
+    this.fileItems = this.fileItems.filter((item) => !item.checked);
+  }
+}
 export class AppComponent {
   title = 'appli';
 }
